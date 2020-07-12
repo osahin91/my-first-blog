88cd7158e0d18742f4e6d4558e6dd11cf5426e4d
Onur <osahin91@gmail.com>
2020-07-12 03:15:54 -0500

    Added forms

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
 	path('', views.post_list, name='post_list'),
+	#looks for /post/<passes post primary key to views as variable pk>/
+	path('post/<int:pk>/', views.post_detail, name='post_detail'),
+	path('post/new/', views.post_new, name='post_new'),
+	path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
